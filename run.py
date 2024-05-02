733cce53456ecb1b10ed71b10e1b1816f83ff817
Dhruvesh Prabhakar <dhruveshprabhakar5354@gmail.com>
2024-05-02 03:34:28 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+#scores = {"computer": 0, "player": 0}
+
+
+
+class gameBoard:
+    """
+    Main game board class. Sets the size of game board.....
+    """
+
+    def __init__(self, size, name):
+        self.size = size
+        self.name = name
+        self.action = []
+        self.board = [[" " for x in range(3)] for y in range(3)]
+
+
+
+
+    
+
